7adfa75dc5ebdeb09b3d1b4a5c17225a1ab95112
Danae321 <167385468+Danae321@users.noreply.github.com>
2024-11-21 19:39:34 -0400

    Update Inicio.jsx

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import Servicios1 from '../components/Servicios1'
+import QuienesSomos1 from '../components/QuienesSomos1'
+import Blog1 from '../components/Blog1'
 import MenuHamburguesa from '../components/MenuHamburguesa'
 import logo from '../images/logo.png'
 
@@ -10,6 +13,9 @@ const Inicio = () => {
         <Header imagenk={logo}></Header>
         <Main ></Main>
         <MenuHamburguesa></MenuHamburguesa>
+        <Servicios1></Servicios1>
+        <QuienesSomos1></QuienesSomos1>
+        <Blog1></Blog1>
     </>
   )
 }
